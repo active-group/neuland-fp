37a327f65c47baf57e77ccbf72e6e1a4c57ab2f6
Mike Sperber <sperber@deinprogramm.de>
2023-05-08 10:57:14 +0200

    Update.

--- vorkurs/data.rkt
+++ vorkurs/data.rkt
@@ -1,1 +1,12 @@
 #lang deinprogramm/sdp/beginner
+
+; Datenmodellierung
+
+; Datendefinition
+
+; Ein Haustier ist eins der folgenden:
+; - Schildkröte -ODER-
+; - Katze -ODER-
+; - Schlange
+; Fallunterscheidung
+; speziell: Aufzählung
