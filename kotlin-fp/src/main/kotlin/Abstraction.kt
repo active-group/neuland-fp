bdc1d9e4ff236351615ac4e7c2df74f640a7ac0d
Mike Sperber <sperber@deinprogramm.de>
2023-05-09 16:42:57 +0200

    Update.

--- kotlin-fp/src/main/kotlin/Abstraction.kt
+++ kotlin-fp/src/main/kotlin/Abstraction.kt
@@ -1,6 +1,12 @@
 // Ein optionaler Wert (vom Typ: A) ist eins der folgenden:
 // - nicht da
 // - oder doch
+
+/*
+interface Functor<C> {
+    fun <A, B> map(f: (A) -> B): C<B>
+}
+ */
 sealed interface Optional<out A> {
     fun <B> map(f: (A) -> B): Optional<B> = optionalMap(f, this)
 }
@@ -35,7 +41,9 @@ fun <A> listIndex(element: A, list: List<A>): Optional<Int> =
                 There(0)
             else {
                 val restIndex = listIndex(element, list.rest)
-                optionalMap({ index -> index + 1 }, restIndex)
+                // optionalMap({ index -> index + 1 }, restIndex)
+                // restIndex.map { index -> index + 1}
+                restIndex.map { it + 1}
 /*                when (restIndex) {
                     is NotThere -> NotThere
                     is There -> There(restIndex.value + 1)
