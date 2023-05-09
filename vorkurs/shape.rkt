fba32d0f8504dc8dfaf31b6cf1d703555862f443
Mike Sperber <sperber@deinprogramm.de>
2023-05-09 10:41:27 +0200

    Update.

--- vorkurs/shape.rkt
+++ vorkurs/shape.rkt
@@ -1,2 +1,13 @@
 #lang deinprogramm/sdp/beginner
 
+; Inspiration: Geometric Region Server
+
+; Geometrische Form ("Shape", "Form"):
+; - Kreis
+; - Quadrat
+; - eine Überlagerung zweiter geometrischer Formen
+
+; Aufgabe:
+; - Datenmodell
+; - paar Beispiele
+; - Funktion, die für einen Punkt ermittelt, ob er innerhalb oder außerhalb einer Form ist.
