28768d40a20d05da9a10546db031c0ce284ea9f2
Mike Sperber <sperber@deinprogramm.de>
2023-05-09 13:45:14 +0200

    Update.

--- vorkurs/warenkorb.rkt
+++ vorkurs/warenkorb.rkt
@@ -1,1 +1,43 @@
 #lang deinprogramm/sdp
+; Warenkörbe
+
+; Lebenszyklus
+; Lieferadresse
+; Gutscheine
+; Zahlart
+; Validierung
+
+; nicht jeden Artikel an jede Adresse
+; nicht jede Zahlart
+
+; - Artikel in den Warenkorb legen
+; - in den Checkout gehen
+; - Lieferart / Zahlart angeben
+; - weiterhin Artikel in den Warenkorb legen
+; - Artikel entfernen
+; - bei Kosmetikartikeln wird die Zahlart Paypal ungültig
+; - (nur gültige) Bestellung abschicken
+
+; Ein Warenkorb ist eins der folgenden:
+; - den Entwurfs-Warenkorb -ODER-
+; - ein bestellfertigen Warenkorb
+
+; Der Entwurfs-Warenkorb besteht aus:
+; - den Artikeln
+; - Kunde ODER NICHT
+; - Lieferadresse ODER NICHT ODER eine unzulässige Lieferadresse
+; - Zahlungsart ODER NICHT ODER eine unzulässige Zahlungsart
+
+; Der bestellfertige Warenkorb besteht aus:
+; - den Artikeln
+; - Kunde
+; - Lieferadresse
+; - Zahlungsart
+(define-record warenkorb-bestellfertig
+  make-warenkorb-bestellfertig
+  warenkorb-bestellfertig?
+  (warenkorb-bestellfertig-artikel (list-of artikel))
+  (warenkorb-bestellfertig-kunde kunde)
+  (warenkorb-bestellfertig-lieferadresse adresse)
+  (warenkorb-bestellfertig-zahlungsart zahlungsart))
+
