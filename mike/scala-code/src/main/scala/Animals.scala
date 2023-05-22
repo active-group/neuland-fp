eb08cd2e9db6b56c5ce5adb598e667b99b473d39
Mike Sperber <sperber@deinprogramm.de>
2023-05-22 10:39:52 +0200

    Update.

--- mike/scala-code/src/main/scala/Animals.scala
+++ mike/scala-code/src/main/scala/Animals.scala
@@ -1,27 +1,43 @@
-// Ein Tier (auf dem texanischen Highway) ist eins der folgenden:
-// - Gürteltier -ODER-
-// - Papagei
-enum Animal {
-  // Ein Gürteltier hat folgende Eigenschaften:
-  // - lebendig oder tot
-  // - Gewicht
-  case Dillo(liveness: Liveness, weight: Weight)
-  case Parrot(sentence: String, weight: Weight)
-  // gemischte Daten / aus zusammengesetzten Daten
+object Animals {
+  // Ein Tier (auf dem texanischen Highway) ist eins der folgenden:
+  // - Gürteltier -ODER-
+  // - Papagei
 
-  def runOver: Animal =
-    this match {
+  enum Animal {
+    // Ein Gürteltier hat folgende Eigenschaften:
+    // - lebendig oder tot
+    // - Gewicht
+    case Dillo(liveness: Liveness, weight: Weight)
+    case Parrot(sentence: String, weight: Weight)
+    // gemischte Daten / aus zusammengesetzten Daten
+
+    def runOver: Animal =
+      this match {
+        case Dillo(_, weight) => Dillo(Liveness.Dead, weight)
+        case Parrot(_, w) => Parrot("", w)
+      }
+  }
+
+  import Animal.*
+
+  def runOver(animal: Animal): Animal = {
+    import Animal.*
+    animal match {
       case Dillo(_, weight) => Dillo(Liveness.Dead, weight)
       case Parrot(_, w) => Parrot("", w)
     }
-}
+  }
 
-type Weight = Double
+  // Aufgabe: Funktion schreiben, die ein Tier füttert.
+  // Tote Gürteltiere nehmen nicht zu.
 
-// "Lebendigkeit" ist eins der folgenden:
-// - lebendig
-// - tot
-enum Liveness {
-  case Alive
-  case Dead
+  type Weight = Double
+
+  // "Lebendigkeit" ist eins der folgenden:
+  // - lebendig
+  // - tot
+  enum Liveness {
+    case Alive
+    case Dead
+  }
 }
