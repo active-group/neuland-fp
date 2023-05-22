c562d48e237c85e022256f8b76abe27768ca79bc
Mike Sperber <sperber@deinprogramm.de>
2023-05-22 11:11:34 +0200

    Update.

--- mike/scala-code/src/main/scala/Lists.scala
+++ mike/scala-code/src/main/scala/Lists.scala
@@ -1,3 +1,21 @@
 object Lists {
-  
+  // Eine Liste ist eins der folgenden:
+  // - die leere Liste
+  // - eine Cons-Liste aus erstem Element und Rest-Liste
+  //                                               ^^^^^
+  enum MyList[+A] {
+    case Empty
+    case Cons(first: A, rest: MyList[A])
+  }
+
+  // Eingebaute Listen:
+  // - leere Liste: Nil
+  // - Cons: :: (Infix)
+
+  def listSum(list: List[Int]): Int =
+    list match {
+      case Nil => 0
+      case first :: rest =>
+        first + listSum(rest)
+    }
 }
