d9ecfcf3cbbb444c3d0106d3533018df197ac4e8
Mike Sperber <sperber@deinprogramm.de>
2023-05-22 14:44:55 +0200

    Update.

--- mike/scala-code/src/main/scala/Algebra.scala
+++ mike/scala-code/src/main/scala/Algebra.scala
@@ -1,1 +1,22 @@
+object Algebra {
+  // Assoziativität
+  // (a + b) + c = a + (b + c)
+  // (a * c) * c = a * (b * c)
 
+  // Halbgruppe:
+  // Typ A
+  // Operation(en):
+  // def f(a1: A, a2: A): A
+  // Gleichungen:
+  // f(f(a, b), c) = f(a, f(b, c))
+
+  trait Semigroup[A] {
+    // combine muß assoziativ sein
+    def combine(a1: A, a2: A): A
+  }
+
+  sealed trait MyBool {
+    case Yo
+    case No
+  }
+}
