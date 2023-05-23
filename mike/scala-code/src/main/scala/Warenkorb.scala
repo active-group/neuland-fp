932be9764c01443bc33d63d76cb729b8c0fbc18e
Mike Sperber <sperber@deinprogramm.de>
2023-05-23 11:49:08 +0200

    Update.

--- mike/scala-code/src/main/scala/Warenkorb.scala
+++ mike/scala-code/src/main/scala/Warenkorb.scala
@@ -57,7 +57,7 @@ enum AttributEntwurf[+T, +GRUND] {
   case AttributNichtDa
 }
 
-given attributEntwurfFunctor: Functor[AttributEntwurf[]]
+given attributEntwurfFunctor[GRUND]: Functor[AttributEntwurf[_;]]
 import AttributEntwurf._
 
 def attributEntwurfMap[A, B, GRUND](f: A => B, entwurf: AttributEntwurf[A, GRUND])
